c36219936e2574a34b2a8302795902fb5eb35037
marcpetrecca1 <marcanthony.petrecca@gmail.com>
2023-01-26 16:11:10 -0500

    objects in ts

---
+++ app.ts
@@ -0,0 +1,12 @@
+// const person: { name: string; age: number } = {
+//   name: 'Max',
+//   age: 30,
+// };
+
+// use type inference
+const person = {
+  name: 'Max',
+  age: 30,
+};
+
+console.log(person.name);
