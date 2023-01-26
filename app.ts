eed9a8d425c57695925f9f0884408bf04d8cac6b
marcpetrecca1 <marcanthony.petrecca@gmail.com>
2023-01-26 16:13:29 -0500

    objects in ts

--- app.ts
+++ app.ts
@@ -10,3 +10,29 @@ const person = {
 };
 
 console.log(person.name);
+
+const product = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!',
+  },
+};
+
+// this object type would be
+
+/*
+{
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  }
+}
+*/
+
+// So you have an object type in an object type so to say.
