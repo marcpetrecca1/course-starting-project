79b1a31adda4211852560295b59be4e9ef8da92c
marcpetrecca1 <marcanthony.petrecca@gmail.com>
2023-01-27 12:21:22 -0500

    type aliases

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-function combine(input1: number | string, input2: number | string) {
+// type alias
+type Combinable = string | number;
+type Descriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable) {
   // workaround at runtime - often times must include this if logic to avoid error in development
   let result: number | string;
   if (typeof input1 === 'number' && typeof input2 === 'number') {
@@ -18,9 +22,9 @@ WORKAROUND - often times must include this if logic to avoid error in developmen
 */
 
 function combining(
-  input1: number | string,
-  input2: number | string,
-  resultType: 'as-number' | 'as-text'
+  input1: Combinable,
+  input2: Combinable,
+  resultType: Descriptor
 ) {
   // resultType is a union type of literal types - the types that are based on your core types (string/number) but then you have a specific version of that type (stricly those two values)
   let result: number | string;
@@ -55,3 +59,30 @@ console.log(combinedStringAges);
 
 const combinedNames = combining('Max', 'Anna', 'as-text');
 console.log(combinedNames);
+
+/*
+Type aliases can be used to "create" your own types. You're not limited to storing union types though - you can also provide an alias to a (possibly complex) object type.
+*/
+
+type User = { name: string; age: number };
+const u1: User = { name: 'Max', age: 30 }; // this works!
+
+/*
+This allows you to avoid unnecessary repetition and manage types centrally.
+*/
+
+// function greet(user: { name: string; age: number }) {
+//   console.log('Hi, I am ' + user.name);
+// }
+
+// function isOlder(user: { name: string; age: number }, checkAge: number) {
+//   return checkAge > user.age;
+// }
+
+function greet(user: User) {
+  console.log('Hi, I am ' + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
